d7d43237a4cc1473c77c37f000434457990f03aa
Ali Raza <shahaliraza298@gmail.com>
2024-07-24 16:33:24 +0500

    New app Added

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Custom Model
+AUTH_USER_MODEL = 'app.CustomUser'
